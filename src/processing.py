79490d6eba6a89ffb7d4497c820de7720e5eec12
Artem Kulinich <kulini4perm@gmail.com>
2025-11-17 16:58:15 +0500

    with sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,16 @@ def filter_by_state(list_of_dicts: list[dict], value_state: str = "EXECUTED") ->
             filtered_list.append(any_dict)
 
     return filtered_list
+
+
+def sort_by_date(list_of_dicts: list[dict], value_date: str = "descending") -> list[dict]:
+    """Функция сортирует список словарей по значению ключа 'date'"""
+
+    if value_date == "descending":  # Направление сортировки: True для убывания, False для возрастания
+        reverse_flag = True
+    else:
+        reverse_flag = False
+    # Используем функцию sorted(), сортируем по значению ключа 'date'
+    sorted_list = sorted(list_of_dicts, key=lambda x: x["date"], reverse=reverse_flag)
+
+    return sorted_list
